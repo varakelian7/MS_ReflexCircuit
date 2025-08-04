bd477a6a2057bf288647581ac5ed8766bca29805
maitreyimadhira <madhiramaitreyi@gmail.com>
2025-08-03 21:34:08 -0400

    params.py

---
+++ params.py
@@ -0,0 +1,18 @@
+ppms_profile = {
+    'demyelination_pct': 0.06,
+    'demyelination_severity': 0.25,
+    'nmda_weight_mult': 1.1,
+    'gaba_b_weight_mult': 0.95
+}
+rrms_attack = {
+    'demyelination_pct': 0.4,  
+    'demyelination_severity': 0.7,
+    'nmda_weight_mult': 1.6,
+    'gaba_b_weight_mult': 0.6
+}
+rrms_remission = {
+    'demyelination_pct': 0.08,  
+    'demyelination_severity': 0.3,
+    'nmda_weight_mult': 1.0,
+    'gaba_b_weight_mult': 1.0
+}
